9f156be8447f7b5cbfd34a6a50059c7e770b1b4c
adam <alucia@communityelf.com>
2017-02-17 15:27:49 -0500

    Add subscription to member info, share view files so the browser can use them, when going to localhost send index file

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 var Members = require('./models/members');
+var path = require('path');
+
+//share the front end files
+app.use("/app", express.static(path.join(__dirname, 'app')));
 
 app.get('/members', cors(), function (req, res) {
-  Members.fetchAll().then(function (members) {
+  Members.fetchAll({withRelated: ['subscription']}).then(function (members) {
     res.json({error: false, data: members.toJSON()});
   })
   .catch(function(error) {
-    console.error(error)
+    console.error(error);
   });
+});
+
+app.all('/', function (req, res) {
+    console.log('Sending to index', __dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 3000;
